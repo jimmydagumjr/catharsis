15493f39447b5b73ccf060aea6682f225fdbcfd7
jimmydagumjr <jimmydagumjr@gmail.com>
2023-12-14 15:46:43 -0800

    added logic for gallery page, still missing css

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,9 +3,20 @@ import { galleryData } from './data/galleryData.jsx';
 export default function Gallery() {
    return (
       galleryData.map( photo => {
+         const formattedTime = photo.time.toLocaleDateString(undefined, {
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+         })
          return (
-            <div className="box">
-               { photo.location }
+            <div className="card">
+               <div className="card__img-container">
+                  <img className="card__img" src={ photo.src } />
+               </div>
+               <div class="card__details">
+                  <h2 class="card__location">{ photo.location }</h2>
+                  <p class="card__time">{ formattedTime }</p>
+               </div>
             </div>
          )
       })
