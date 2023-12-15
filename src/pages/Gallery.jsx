8e09443140fc99761a43b5a5612e3b9a56c2e0ae
jimmydagumjr <jimmydagumjr@gmail.com>
2023-12-15 15:41:52 -0800

    finished rough gallery; need to implement lazy loading and infinite scroll for chunks of pictures

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,24 +1,39 @@
-import { galleryData } from './data/galleryData.jsx';
+import GalleryCSS from "./../assets/css/Gallery.module.css"
+import { galleryData } from './data/galleryData.jsx'
 
 export default function Gallery() {
    return (
-      galleryData.map( photo => {
-         const formattedTime = photo.time.toLocaleDateString(undefined, {
-            year: "numeric",
-            month: "long",
-            day: "numeric"
-         })
-         return (
-            <div className="card">
-               <div className="card__img-container">
-                  <img className="card__img" src={ photo.src } />
-               </div>
-               <div class="card__details">
-                  <h2 class="card__location">{ photo.location }</h2>
-                  <p class="card__time">{ formattedTime }</p>
-               </div>
-            </div>
-         )
-      })
+      <div className={GalleryCSS.photo_body}>
+         <div className={GalleryCSS.photo_container}>
+            <>{
+               galleryData.map(photo => {
+                  // time formatting
+                  const formattedTime = photo.time.toLocaleDateString(undefined, {
+                     year: "numeric",
+                     month: "long",
+                     day: "numeric"
+                  })
+                  // card output
+                  return (
+                     <div className={GalleryCSS.photo_body}>
+                        <div className={GalleryCSS.photo_container}>
+
+                           <div className={GalleryCSS.card}>
+                              <div className={GalleryCSS.card__img_container}>
+                                 <img className={GalleryCSS.card__img} src={photo.src} />
+                              </div>
+                              <div className={GalleryCSS.card__details}>
+                                 <h2 className={GalleryCSS.card__location}>{photo.location}</h2>
+                                 <p className={GalleryCSS.card__time}>{formattedTime}</p>
+                              </div>
+                           </div>
+
+                        </div>
+                     </div>
+                  )
+               })
+            }</>
+         </div>
+      </div>
    );
 }
--- src/pages/data/galleryData.jsx
+++ src/pages/data/galleryData.jsx
@@ -1,117 +1,117 @@
-export const galleryData = [  
+export const galleryData = [
     {
         location: "scv, ca",
         time: new Date("8/20/23"),
-        src: "photos/IMG-3472.jpg"
+        src: "src/pages/data/photos/IMG-3472.jpg"
     },
     {
         location: "home, nv",
         time: new Date("8/2/23"),
-        src: "photos/IMG_3302.jpg"
+        src: "src/pages/data/photos/IMG_3302.jpg"
     },
     {
         location: "tivoli village, nv",
         time: new Date("7/19/23"),
-        src: "photos/IMG_8755.jpg"
+        src: "src/pages/data/photos/IMG_8755.jpg"
     },
     {
         location: "met museum, ny",
         time: new Date("4/24/23"),
-        src: "photos/IMG_2392.jpg"
+        src: "src/pages/data/photos/IMG_2392.jpg"
     },
     {
         location: "subway, ny",
         time: new Date("4/21/23"),
-        src: "photos/DSC00888_Original.jpg"
+        src: "src/pages/data/photos/DSC00888_Original.jpg"
     },
     {
         location: "chinatown, ny",
         time: new Date("4/21/23"),
-        src: "photos/IMG_9386.jpg"
+        src: "src/pages/data/photos/IMG_9386.jpg"
     },
     {
         location: "luigi's pizza, ny",
         time: new Date("4/21/23"),
-        src: "photos/DSC00865_Original.jpg"
+        src: "src/pages/data/photos/DSC00865_Original.jpg"
     },
     {
         location: "subway, ny",
         time: new Date("4/20/23"),
-        src: "photos/IMG_2218.jpg"
+        src: "src/pages/data/photos/IMG_2218.jpg"
     },
     {
         location: "home, nv",
         time: new Date("2/3/23"),
-        src: "photos/IMG_1542.jpg"
+        src: "src/pages/data/photos/IMG_1542.jpg"
     },
     {
         location: "childhood photobook",
         time: new Date("12/11/22"),
-        src: "photos/IMG_0943.jpg"
+        src: "src/pages/data/photos/IMG_0943.jpg"
     },
     {
         location: "2nd concert, nv",
         time: new Date("11/7/22"),
-        src: "photos/IMG_3171.jpg"
+        src: "src/pages/data/photos/IMG_3171.jpg"
     },
     {
         location: "first friday, nv",
         time: new Date("10/7/22"),
-        src: "photos/IMG_2363.jpg"
+        src: "src/pages/data/photos/IMG_2363.jpg"
     },
     {
         location: "arts district, nv",
         time: new Date("8/26/22"),
-        src: "photos/51872C29-5206-43C0-9CE0-81EFBD526E18.jpg"
+        src: "src/pages/data/photos/51872C29-5206-43C0-9CE0-81EFBD526E18.jpg"
     },
     {
         location: "1st concert, ca",
         time: new Date("7/31/22"),
-        src: "photos/IMG_5795.jpg"
+        src: "src/pages/data/photos/IMG_5795.jpg"
     },
     {
         location: "paramount, ca",
         time: new Date("7/31/22"),
-        src: "photos/IMG_5790.jpg"
+        src: "src/pages/data/photos/IMG_5790.jpg"
     },
     {
         location: "neighborhood park, oh",
         time: new Date("7/15/22"),
-        src: "photos/67961315162__102C2081-E411-4A56-A443-155D3F2C43FB.jpg"
+        src: "src/pages/data/photos/67961315162__102C2081-E411-4A56-A443-155D3F2C43FB.jpg"
     },
     {
         location: "oregon district, oh",
         time: new Date("7/1/22"),
-        src: "photos/IMG_8420.jpg"
+        src: "src/pages/data/photos/IMG_8420.jpg"
     },
     {
         location: "oregon district, oh",
         time: new Date("7/1/22"),
-        src: "photos/IMG_8377.jpg"
+        src: "src/pages/data/photos/IMG_8377.jpg"
     },
     {
         location: "neighborhood park, oh",
         time: new Date("6/24/22"),
-        src: "photos/IMG_8248.jpg"
+        src: "src/pages/data/photos/IMG_8248.jpg"
     },
     {
         location: "englewood, oh",
         time: new Date("6/11/22"),
-        src: "photos/IMG_2503.jpg"
+        src: "src/pages/data/photos/IMG_2503.jpg"
     },
     {
         location: "englewood, oh",
         time: new Date("6/11/22"),
-        src: "photos/IMG_8088.jpg"
+        src: "src/pages/data/photos/IMG_8088.jpg"
     },
     {
         location: "first friday, nv",
         time: new Date("5/6/22"),
-        src: "photos/IMG_1396.jpg"
+        src: "src/pages/data/photos/IMG_1396.jpg"
     },
     {
         location: "first friday, nv",
         time: new Date("3/4/22"),
-        src: "photos/IMG_6240.jpg"
+        src: "src/pages/data/photos/IMG_6240.jpg"
     },
 ];
