bb15fc95d41d7147178efa8dd9f35ecb4609e7a6
jimmy <jimmydagumjr@gmail.com>
2023-12-16 22:52:35 -0800

    added framer motion and animations to photobook

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,12 +1,27 @@
 import GalleryCSS from "./../assets/css/Gallery.module.css"
 import { galleryData } from './data/galleryData.jsx'
+import { motion } from "framer-motion"
+
+const cardVariants = {
+   hidden: {
+      opacity: 0,
+      y: 100
+   },
+   visible: (index) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+         delay: 0.03 * index
+      }
+   }),
+}
 
 export default function Gallery() {
    return (
       <div className={GalleryCSS.photo_body}>
          <div className={GalleryCSS.photo_container}>
             <>{
-               galleryData.map(photo => {
+               galleryData.map((photo, index) => {
                   // time formatting
                   const formattedTime = photo.time.toLocaleDateString(undefined, {
                      year: "numeric",
@@ -15,21 +30,28 @@ export default function Gallery() {
                   })
                   // card output
                   return (
-                     <div className={GalleryCSS.photo_body}>
-                        <div className={GalleryCSS.photo_container}>
-
-                           <div className={GalleryCSS.card}>
-                              <div className={GalleryCSS.card__img_container}>
-                                 <img className={GalleryCSS.card__img} src={photo.src} />
-                              </div>
-                              <div className={GalleryCSS.card__details}>
-                                 <h2 className={GalleryCSS.card__location}>{photo.location}</h2>
-                                 <p className={GalleryCSS.card__time}>{formattedTime}</p>
-                              </div>
-                           </div>
-
+                     <motion.div 
+                     className={GalleryCSS.card}
+                     variants={cardVariants}
+                     initial="hidden"
+                     whileInView="visible"
+                     viewport={{
+                        once: true
+                     }}
+                     custom={index}
+                     >
+                        <div className={GalleryCSS.card__img_container}>
+                           <img 
+                           className={GalleryCSS.card__img} 
+                           src={photo.src} 
+                           loading="lazy"
+                           />
                         </div>
-                     </div>
+                        <div className={GalleryCSS.card__details}>
+                           <h2 className={GalleryCSS.card__location}>{photo.location}</h2>
+                           <p className={GalleryCSS.card__time}>{formattedTime}</p>
+                        </div>
+                     </motion.div>
                   )
                })
             }</>
