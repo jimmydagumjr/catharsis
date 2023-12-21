7a2911f20bd8e826aa5b25b22debdfa272de3870
jimmy <jimmydagumjr@gmail.com>
2023-12-20 21:38:27 -0800

    set up skeleton of audio player and styling

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,42 +1,66 @@
+import React from "react"
 import MusicPlayerCSS from "./../assets/css/MusicPlayer.module.css"
 import AudioPlayer, { RHAP_UI } from "react-h5-audio-player"
-// import 'react-h5-audio-player/lib/styles.css'
 import './../assets/css/AudioPlayer.scss'
+import customIcons from "./../assets/svgs/CustomIcons.jsx"
 
 const MusicPlayer = () => {
     return (
-
         <div className={MusicPlayerCSS.playerContainer}>
             <div className={MusicPlayerCSS.playerFilter}>
-                <AudioPlayer
-                    // className={MusicPlayerCSS.player}
+                <CustomAudioPlayer
                     src="https://files.catbox.moe/w5scaz.mp3"
-                    style={{
-                        borderRadius: "1rem",
-                        position: "sticky",
-                        boxShadow: "none",
-                        backgroundColor: "transparent",
-                        fontFamily: "Courier"
-                    }}
-                    // layout='stacked-reverse'
+                    layout='stacked-reverse'
                     showSkipControls={true}
                     showJumpControls={false}
                     showFilledVolume={true}
-                    customProgressBarSection={
+                    showDownloadProgress={false}
+                    customAdditionalControls={
                         [
-                            // RHAP_UI.CURRENT_TIME,
-
-                            RHAP_UI.CURRENT_TIME,
-                            RHAP_UI.PROGRESS_BAR,
-                            RHAP_UI.CURRENT_LEFT_TIME,
-                            RHAP_UI.VOLUME
+                            <button>
+                                s
+                            </button>
                         ]
                     }
+                    customProgressBarSection={
+                        [
+                            RHAP_UI.CURRENT_TIME,
+                            RHAP_UI.PROGRESS_BAR,
+                            RHAP_UI.CURRENT_LEFT_TIME
+                        ]
+                    }
+                    customControlsSection={
+                        [
+                            RHAP_UI.ADDITIONAL_CONTROLS,
+                            RHAP_UI.MAIN_CONTROLS,
+                            RHAP_UI.LOOP
+                        ]
+                    }
+                    customIcons={customIcons}
+                    autoPlayAfterSrcChange={false}
                     customVolumeControls={[]}
                 />
             </div>
         </div>
     )
 }
+
+class CustomAudioPlayer extends React.Component {
+    render() {
+        return (
+            <div>
+                <AudioPlayer {...this.props} />
+                {/* <div>
+                    <p>test</p>
+                </div> */}
+                {/* make container of audioplayer and these items
+                into a flexbox? similar to navbar and have song/artist on left,
+                and volume on right, use Advanced Usage section on github*/}
+            </div>
+        )
+    }
+}
+
+
 
 export default MusicPlayer
