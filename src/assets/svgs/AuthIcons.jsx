3122070910dea06929662e12293728694d9b245c
jimmydagumjr <jimmydagumjr@gmail.com>
2024-01-10 14:41:28 -0800

    some styling

--- src/assets/svgs/AuthIcons.jsx
+++ src/assets/svgs/AuthIcons.jsx
@@ -41,8 +41,8 @@ export const PasswordIcon = () => {
 export const GithubIcon = () => {
   return (
     <svg
-      width="28"
-      height="28"
+      width="22"
+      height="22"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
     >
